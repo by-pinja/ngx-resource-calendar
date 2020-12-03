b474240bff7c3d510c017bc09505d6de64c2aea7
Sami Kauranen <sami.kauranen@pinja.com>
2020-12-03 13:04:42 +0200

    Updated angular to version 11

--- projects/ngx-resource-calendar/src/lib/resource-calendar.component.spec.ts
+++ projects/ngx-resource-calendar/src/lib/resource-calendar.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ResourceCalendarComponent } from './resource-calendar.component';
 
@@ -6,7 +6,7 @@ describe('ResourceCalendarComponent', () => {
   let component: ResourceCalendarComponent;
   let fixture: ComponentFixture<ResourceCalendarComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ResourceCalendarComponent ]
     })
