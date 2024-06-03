21a36de0e30ffe57261e092053c067d87313bf33
ptcs-juma <marko.jurvainen@pinja.com>
2024-06-03 16:06:06 +0300

    Add comments.

--- projects/ngx-resource-calendar/src/lib/models/calendar-event.model.ts
+++ projects/ngx-resource-calendar/src/lib/models/calendar-event.model.ts
@@ -1,7 +1,16 @@
 export class CalendarEventModel<T> {
+  // Event model's original data.
   data: T;
+
+  // Display height in pixels.
   height: number;
+
+  // Y-position on the calendar.
   position: number;
+
+  // Display width in pixels.
   width: string;
+
+  // Left margin.
   left: string;
 }
